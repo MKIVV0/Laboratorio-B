82914ec457ce83771e6456400746700bd4ae75de
Matt <huangzhangying@live.it>
2022-11-14 21:49:35 +0100

    client and server classes modified

--- emotionalsongs/serverES.java
+++ emotionalsongs/serverES.java
@@ -1,5 +1,22 @@
+package emotionalsongs;
+
+import java.io.IOException;
+import java.net.*;
+
 public class serverES {
+
+  public static final int PORT = 13333;
+  static ServerSocket server;
+  static Socket connection = null;
+
   public static void main(String[] args) {
-    
+    while (true) {
+      try {
+        connection = server.accept();
+
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
   }
 }
--- user/clientES.java
+++ user/clientES.java
@@ -1,5 +1,10 @@
+package user;
+
+import java.net.*;
+
 public class clientES {
-  public static void main(String[] args) {
+    public static final InetAddress address = InetAddress.getByName("localhost");
+    public static void main(String[] args) {
     
   }
 }
