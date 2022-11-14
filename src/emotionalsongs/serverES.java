82cccefe3babf1723a33ff3ae433e77468ebcc08
Matt <huangzhangying@live.it>
2022-11-14 22:12:50 +0100

    client and server classes base structure implemented

--- src/emotionalsongs/serverES.java
+++ src/emotionalsongs/serverES.java
@@ -1,18 +1,33 @@
 package emotionalsongs;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.*;
 
 public class serverES {
 
   public static final int PORT = 13333;
   static ServerSocket server;
-  static Socket connection = null;
+  static Socket connection;
+  public static PrintWriter toClient;
+  public static BufferedReader fromClient;
 
   public static void main(String[] args) {
     while (true) {
       try {
+        server = new ServerSocket(PORT);
         connection = server.accept();
+        toClient = new PrintWriter(new OutputStreamWriter(connection.getOutputStream()),true);
+        fromClient = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+        int cnt = 0;
+        while (cnt <= 5) {
+          toClient.println("Sono il server: prova " + cnt++ * 10);
+        }
+
+        String str = null;
+        while ((str = fromClient.readLine()) != null) {
+          System.out.println("MSG FROM CLIENT: " + str);
+        }
 
       } catch (IOException e) {
         e.printStackTrace();
--- src/user/clientES.java
+++ src/user/clientES.java
@@ -1,10 +1,45 @@
 package user;
 
+import emotionalsongs.serverES;
+
+import java.io.*;
 import java.net.*;
 
 public class clientES {
-    public static final InetAddress address = InetAddress.getByName("localhost");
-    public static void main(String[] args) {
-    
+    public static InetAddress address;
+    public static PrintWriter toServer;
+    public static BufferedReader fromServer;
+    public static Socket socket;
+
+    public static void main(String[] args) throws IOException {
+        try {
+            address = InetAddress.getByName("localhost");
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            socket = new Socket(address, serverES.PORT);
+            toServer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()),true);
+            fromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        // FUNZIONA
+        int cnt = 0;
+        while (cnt <= 5) {
+            toServer.println("Sono il client: prova " + cnt++);
+        }
+
+        String str = null;
+        while ((str = fromServer.readLine()) != null) {
+            System.out.println("MSG FROM SERVER: " + str);
+        }
+
+        toServer.close();
+        fromServer.close();
+        socket.close();
   }
 }
