cc0c41e8ef46cf45ccd64052f4f4fe3ab248e564
Natelil <diloalessandro@gmail.com>
2022-12-11 17:01:42 +0100

    test debug

--- src/main/java/user/BarraStrumenti.java
+++ src/main/java/user/BarraStrumenti.java
@@ -84,6 +84,84 @@ public class BarraStrumenti extends JPanel {
             }
         });
 
+        bottoneRegistra.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(bottoneLog.getText().equals("Registrati")) {
+                    JPanel panel = new JPanel(new BorderLayout(5, 5));
+
+                    JPanel label = new JPanel(new GridLayout(0, 1, 2, 2));
+                    label.add(new JLabel("First name", SwingConstants.RIGHT));
+                    label.add(new JLabel("Last name", SwingConstants.RIGHT));
+                    label.add(new JLabel("Fiscal code", SwingConstants.RIGHT));
+                    label.add(new JLabel("Address", SwingConstants.RIGHT));
+                    label.add(new JLabel("E-Mail", SwingConstants.RIGHT));
+                    label.add(new JLabel("Username", SwingConstants.RIGHT));
+                    label.add(new JLabel("Password", SwingConstants.RIGHT));
+
+                    panel.add(label, BorderLayout.WEST);
+
+                    JPanel controls = new JPanel(new GridLayout(0, 1, 2, 2));
+                    JTextField firstname = new JTextField();
+                    JTextField lastname = new JTextField();
+                    JTextField fiscalcode = new JTextField();
+                    JTextField address = new JTextField();
+                    JTextField email = new JTextField();
+                    JTextField username = new JTextField();
+                    JPasswordField password = new JPasswordField();
+                    controls.add(firstname);
+                    controls.add(lastname);
+                    controls.add(fiscalcode);
+                    controls.add(address);
+                    controls.add(email);
+                    controls.add(username);
+                    controls.add(password);
+
+                    panel.add(controls, BorderLayout.CENTER);
+
+                    int scelta = JOptionPane.showConfirmDialog(Frame.getFrames()[0], panel, "registrazione", JOptionPane.OK_CANCEL_OPTION);
+
+                    if (scelta == 0) {
+                        String fn = username.getText();
+                        String ln = username.getText();
+                        String FC = username.getText();
+                        String addr = username.getText();
+                        String em = username.getText();
+                        String uid = username.getText();
+                        String pw = new String(password.getPassword());
+                        if(uid.equals("") || pw.equals("")) {
+                            JOptionPane.showMessageDialog(null, "Please insert Username and Password");
+                        }
+                        else {
+                            try {
+                                frame.user = frame.resourceManager.login(frame.user, username.getText(), new String(password.getPassword()));
+                                JOptionPane.showMessageDialog(null, "Logged in!");
+                            } catch (RemoteException ex) {
+                            } catch (AlreadyLoggedException ex) {
+                            } catch (SQLException ex) {
+                            }catch (WrongCredentialsException ex){
+                                JOptionPane.showMessageDialog(null, "Wrong Username / Password");
+                            }
+                        }
+                    }
+                }else {
+                    try {
+                        frame.user = frame.resourceManager.logout(frame.user);
+                        JOptionPane.showMessageDialog(null, "Logged out!");
+                    }catch (Exception exe){}
+                }
+
+                if(frame.user instanceof LoggedUser) {
+                    bottoneLog.setText("Logout");
+                    bottoneRegistra.setText("+ Playlist");
+                }
+                else if(frame.user instanceof NotLoggedUser) {
+                    bottoneLog.setText("Login");
+                    bottoneRegistra.setText("Registrati");
+                }
+            }
+        });
+
         bottoneLog.setFocusable(false);
         bottoneRegistra.setFocusable(false);
 
